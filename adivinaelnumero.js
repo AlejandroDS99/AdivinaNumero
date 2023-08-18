039dda66d338b4b87936f77f0373f02866fed7e4
Alejandro Diaz <alejandrodiazsal99@gmail.com>
2023-08-18 17:47:41 -0600

    Avances

---
+++ adivinaelnumero.js
@@ -0,0 +1,59 @@
+/* function encuentraNumero(n) {
+    let inicio = 0, fin = 100, intentoUno = 50;
+    while ( n != intentoUno ) {
+        console.log( intentoUno);
+        if ( n < intentoUno ) {
+            fin = intentoUno
+        }
+        else { 
+            inicio = intentoUno; 
+        }
+        intentoUno = Math.floor(( inicio + fin ) /2);
+    }
+    console.log( "si, estees tu numero " + n);
+}
+
+encuentraNumero ( 73 ); */
+
+/* function encuentraNumero(n) {
+    let inicio = 0, fin = 100, intentoUno = 50;
+    alert ("Piensa en un número");
+    while ( n != intentoUno ) {
+        console.log( intentoUno);
+        if ( n < intentoUno ) {
+            fin = intentoUno
+        }
+        else { 
+            inicio = intentoUno; 
+        }
+        intentoUno = Math.floor(( inicio + fin ) /2);
+    }
+    console.log( "si, estees tu numero " + n);
+}
+
+encuentraNumero ( 73 ); */
+
+
+function adivinaNumero() {
+    alert ("Piensa en un número del 0 al 100");
+    let inicio = 0, fin = 101;
+    let acierto = false;
+    do {
+        let intento = Math.floor(( inicio + fin ) /2 ); 
+        acierto = confirm ("¿Es éste tu número? " + intento );
+        if (acierto ){
+            alert ("Gran elección, tu numero es " + intento );
+        }
+        else {
+            let esMenor = confirm ("Tu número ¿es menor?");
+            if ( esMenor) {
+                fin = intento;
+            }
+            else {
+                inicio = intento;
+            }
+        }
+    } while ( !acierto );
+}
+
+
